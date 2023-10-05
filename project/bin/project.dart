402a3a66face5c8d154ae276f1cec2feae215118
Mochamad Ridwan <ridwansyah1933@gmail.com>
2023-10-06 00:32:38 +0700

    Menyambungkan 3 buah string dan menampilkannya Line 11-15

--- project/bin/project.dart
+++ project/bin/project.dart
@@ -2,8 +2,16 @@ import 'package:project/project.dart' as project;
 
 void main() {
   // Implementasi menghitung Luas lingkaran
-  const phi = 3.14;
-  int r = 10;
-  double luas = phi * r * r;
-  print("Luas lingkaran adalah $luas cm");
+  // const phi = 3.14;
+  // int r = 10;
+  // double luas = phi * r * r;
+  // print("Luas lingkaran adalah $luas cm");
+
+  //Menyambungkan 3 buah string
+  String kata1 = "Hallo ";
+  String kata2 = "Saya ";
+  String kata3 = "Mridwan";
+
+  String merge = kata1 + kata2 + kata3;
+  print(merge);
 }
