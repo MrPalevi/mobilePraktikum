4bc5ff5aada34400a256bd6e97927c4a35fa98b7
Mochamad Ridwan <ridwansyah1933@gmail.com>
2023-10-06 00:51:10 +0700

    Tampilkan Factorial 10,20,30 Line 19-37

--- project/bin/project.dart
+++ project/bin/project.dart
@@ -8,10 +8,31 @@ void main() {
   // print("Luas lingkaran adalah $luas cm");
 
   //Menyambungkan 3 buah string
-  String kata1 = "Hallo ";
-  String kata2 = "Saya ";
-  String kata3 = "Mridwan";
+  // String kata1 = "Hallo ";
+  // String kata2 = "Saya ";
+  // String kata3 = "Mridwan";
 
-  String merge = kata1 + kata2 + kata3;
-  print(merge);
+  // String merge = kata1 + kata2 + kata3;
+  // print(merge);
+
+  //Menampilkan factorial dari 10, 20,30
+  int nilai1 = 10;
+  int nilai2 = 20;
+  int nilai3 = 30;
+
+  BigInt faktorial1 = hitungFaktorial(BigInt.from(nilai1));
+  BigInt faktorial2 = hitungFaktorial(BigInt.from(nilai2));
+  BigInt faktorial3 = hitungFaktorial(BigInt.from(nilai3));
+
+  print("$nilai1! adalah $faktorial1");
+  print("$nilai2! adalah $faktorial2");
+  print("$nilai3! adalah $faktorial3");
+}
+
+BigInt hitungFaktorial(BigInt n) {
+  if (n == BigInt.zero) {
+    return BigInt.one;
+  } else {
+    return n * hitungFaktorial(n - BigInt.one);
+  }
 }
